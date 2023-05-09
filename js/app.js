64c15486dc0d07bd3c4f18e7436dc89ec3ac1357
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2023-05-09 06:13:04 -0700

    responsive is done

--- js/app.js
+++ js/app.js
@@ -19,3 +19,37 @@ let calcScrollValue = () => {
   };
   window.onscroll = calcScrollValue;
   window.onload = calcScrollValue;
+
+  // BURGER MENU
+
+  
+  let elCloseBtn = document.querySelector(".burger-close");
+let elNavMenu = document.querySelector(".burger-nav");
+let elBurgerBtn = document.querySelector(".burger-btn");
+let elBurgerLinks = document.querySelectorAll(".site-burger__link");
+
+elCloseBtn.addEventListener("click", function() {
+  elNavMenu.classList.remove("slide-in");
+  elNavMenu.classList.add("slide-out");
+  setTimeout(() => {
+    elNavMenu.classList.add("hidden");
+    elNavMenu.classList.remove("slide-out");
+  }, 300);
+});
+
+elBurgerBtn.addEventListener("click", function() {
+  elNavMenu.classList.toggle("hidden");
+  elNavMenu.classList.toggle("slide-in");
+  elNavMenu.classList.toggle("display")
+});
+
+elBurgerLinks.forEach(function(elBurgerLink) {
+  elBurgerLink.addEventListener("click", function() {
+    elNavMenu.classList.remove("slide-in");
+    elNavMenu.classList.add("slide-out");
+    setTimeout(() => {
+      elNavMenu.classList.add("hidden");
+      elNavMenu.classList.remove("slide-out");
+    }, 300);
+  });
+});
